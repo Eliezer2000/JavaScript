2741e430387c7637f4214e16ef75d00a41ebf6b0
Eliézer Alves Bernardino <eliezeralvesbernardino@gmail.com>
2022-03-08 15:40:54 -0300

    Alura JavaScript +

---
+++ CursoAlura-Js/tipos-variaveis-funcoes/conversoes.js
@@ -0,0 +1,12 @@
+//Tipo de dados
+//boleanos
+
+//Conversao implicita
+const numero = 436
+const numeroString = "436"
+console.log(numero + numeroString)
+
+//Conversao explicita
+//Number
+//String
+console.log(numero + Number(numeroString))
--- CursoAlura-Js/tipos-variaveis-funcoes/typeBoolean.js
+++ CursoAlura-Js/tipos-variaveis-funcoes/typeBoolean.js
@@ -8,3 +8,26 @@ console.log(primeiroNumero === segundoNúmero)
 const texto1 = 'Alura'
 const texto2 = 'alura'
 console.log( texto1 === texto2)
+
+const minhaVar = 1;
+const MinhaVar = "texto";
+const minhavar = "3";
+const MINHAVAR = 2;
+
+console.log(minhaVar, MinhaVar, minhavar, MINHAVAR)
+
+let input = null;
+
+if (input === null) {
+ console.log('não há informação');
+} else {
+ console.log(input);
+}
+let input = null;
+let input2;
+
+console.log(input); // null
+console.log(input2); // undefined
+
+console.log(null == undefined); // true
+console.log(null === undefined); // false
